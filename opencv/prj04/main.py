ecad00cfd0bc004251136ec96b2be5ca6cf097ea
DESKTOP-9NRVOPM\user1 <hskwon1133@gmail.com>
2026-09-02 09:44:39 +0900

    이미지 : gray 변경, 컵 자르고, 이미지에 컵 넣기.

--- opencv/prj04/main.py
+++ opencv/prj04/main.py
@@ -1,15 +1,19 @@
 import cv2
 
-img = cv2.imread('images/53.png')
-print(img.shape)
+img = cv2.imread('images/53.png', cv2.IMREAD_GRAYSCALE) # Original 일 경우 shape(1086, 1448, 3) IMREAD_GRAYSCALE입력하면 채널은 없음 (1086, 1448)
+# print(img.shape)
 
 #잘라서 색을 바꿔봄
-roi  = img[0:100,0:100,:]
-roi[0:100, 0:100] = [0,255,0]
+# roi  = img[0:100,0:100,:]
+# roi[0:100, 0:100] = [0,255,0]
+#
+# print(roi.shape)
 
-print(roi.shape)
+roi = img[550:900, 470:830] #컵만 자르기
+img[0:350, 0:360] = roi # img에 roi넣기
 
-
+cv2.imshow('roi',roi)
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
